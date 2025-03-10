9ff127e3cd47a888f450c9d5929b39faad45b013
Vaibhav Sain <155076216+VaibhavSain@users.noreply.github.com>
2025-03-10 14:13:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const rate1 = document.getElementById("rate1");
+const rate2 = document.getElementById("rate2");
+const rate3 = document.getElementById("rate3");
+const rate4 = document.getElementById("rate4");
+const rate5 = document.getElementById("rate5");
+const rate6 = document.getElementById("rate6");
+const rate7 = document.getElementById("rate7");
+
+function mon () {
+    rate1.style.display = "block"; 
+}
+function tue () {
+    rate2.style.display = "block";
+}
+function wed () {
+    rate3.style.display = "block";
+}
+function thu () {
+    rate4.style.display = "block";
+}
+function fri() {
+    rate5.style.display = "block";
+}
+function sat() {
+    rate6.style.display = "block";
+}
+function sun() {
+    rate7.style.display = "block";
+}
+function hide() {
+    rate1.style.display = "none";
+    rate2.style.display = "none";
+    rate3.style.display = "none";
+    rate4.style.display = "none";
+    rate5.style.display = "none";
+    rate6.style.display = "none";
+    rate7.style.display = "none";
+}
